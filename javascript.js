3429225b723c803143971a0559b1114dc395fe49
Roman Lukianenko <142048423+RomanLukyanenko@users.noreply.github.com>
2023-09-13 01:51:36 +0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+//Create a function that reverses str
 function reverseString(str) {
     let reversed = '';
     for (let i = str.length - 1; i >= 0; i--) {
@@ -7,4 +8,19 @@ function reverseString(str) {
 }
 console.log(reverseString('hello'))
 
-jj
+
+
+// Create a function that clears spaces
+const clearWhiteSpace = (str) => {
+    return str.split(' ').join('');
+};
+
+console.log(clearWhiteSpace('hello 1 1 1 2'));
+
+
+
+//Create a function that counts words in sentence
+const countWords = (str) => {
+    return str.split(' ').length;
+};
+console.log(countWords('Hi my name is Taras'))
