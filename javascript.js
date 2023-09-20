2267592b2871a58510653e2e4b73e0417add980b
Roman Lukianenko <142048423+RomanLukyanenko@users.noreply.github.com>
2023-09-20 18:10:48 +0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -73,17 +73,17 @@ const obj = {
 
   const calculator = {
     result: 0,
-  
+      
     sum: function(value) {
       this.result += value;
       return this;
     },
-  
+      
     sub: function(value) {
       this.result -= value;
       return this;
     },
-  
+      
     mul: function(value) {
       this.result *= value;
       return this;
@@ -99,3 +99,58 @@ const obj = {
   console.log(calculator.result);
 
 
+
+
+
+
+
+
+
+const checkFlush = (cards) => {
+    let suits = cards.map(card => card.slice(-1));
+    let uniqueSuits = [...new Set(suits)];
+    return uniqueSuits.length === 1;
+  };
+  
+  const checkSequence = (cards) => {
+    let order = "A2345678910JQK";
+    let cardValues = cards.map(card => order.indexOf(card.slice(0, -1))).sort((a, b) => a - b);
+  
+    let uniqueCardValues = [...new Set(cardValues)];
+    if (uniqueCardValues.length !== 5) {
+        return false;
+    }
+  
+    if(cardValues.join('') === '01291011') {
+        return true;
+    }
+  
+    return cardValues[4] - cardValues[0] === 4;
+  };
+  
+  function Poker(players, names) {
+    this.players = players;
+    this.names = names;
+
+    let suits = ['♦', '♣', '♥', '♠'];
+    let values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+  
+    let deck = [];
+    for (let suit of suits) {
+        for (let value of values) {
+            deck.push(value + suit);
+        }
+    }
+
+    deck.sort(() => Math.random() - 0.5);
+  
+    this.hands = {};
+    for (let name of this.names) {
+        this.hands[name] = deck.splice(0, 5);
+    }
+  }
+  
+  let game = new Poker(2, ["Player1", "Player2"]);
+  console.log(game.hands);
+  console.log(checkFlush(game.hands["Player1"]));
+  console.log(checkSequence(game.hands["Player1"]));
